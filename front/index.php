1bb925580f3871953b710603e233ca53704f0d7e
InformaticNuke <InformaticNuke@gmail.com>
2023-06-27 13:03:45 -0400

    v1

---
+++ front/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1>Bienvndo usuario</h1>
+    
+</body>
+
+<footer>
+      <p>&copy; InfomaticNuke</p>
+      <p>
+        <a href="https://www.instagram.com/informaticnuke/" target="_blank">Instagram | </a>
+        <a href="https://twitter.com/InformaticNuke" target="_blank">Twitter | </a>
+        <a href="https://www.reddit.com/user/InformaticNuke" target="_blank">Reddit | </a>
+        <a href="https://steamcommunity.com/id/InformaticNuke/" target="_blank">Steam | </a>
+        <a href="https://discord.gg/vkBm8EQ" target="_blank">Discord</a>
+      </p>
+</footer>
+
+</html>
---
+++ front/logscreen.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login • Intranet</title>
+    <link rel="stylesheet" href="style\style.css">
+    <link rel="icon" href="img\logoVieja2-modified.png" type="image/x-icon">
+
+</head>
+
+<body>
+    <div class="login">
+
+        <form action="Bcknd/log.php" method="post">
+            <label for="Rut">Rut</label>
+            <br>
+            <input type="text" name="rut">
+            <br><br> 
+            <label for="Rut">Password</label>
+            <br>
+            <input type="password" name="pass">
+            <br><br><br><br>
+            <input type="submit" value="Login">
+        </form>
+
+    </div>
+</body>
+</html>
